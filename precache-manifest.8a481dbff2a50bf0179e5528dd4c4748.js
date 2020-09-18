661f0ae2f02adecbebbca4d0ec951d08204f9b97
Hamza Farooqui <hamzattking@gmail.com>
2020-09-18 23:18:20 +0500

    Updates

---
+++ precache-manifest.8a481dbff2a50bf0179e5528dd4c4748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261e472837761b8f3b483f4f44c94f94",
+    "url": "/monster-rolodex-app/index.html"
+  },
+  {
+    "revision": "9ef09bc63659e318bbed",
+    "url": "/monster-rolodex-app/static/css/main.8ceaafd3.chunk.css"
+  },
+  {
+    "revision": "894c651be887db44746a",
+    "url": "/monster-rolodex-app/static/js/2.6aa05017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex-app/static/js/2.6aa05017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef09bc63659e318bbed",
+    "url": "/monster-rolodex-app/static/js/main.b515f69b.chunk.js"
+  },
+  {
+    "revision": "3959ab9e476959a0238a",
+    "url": "/monster-rolodex-app/static/js/runtime-main.ab0a2a9b.js"
+  }
+]);
